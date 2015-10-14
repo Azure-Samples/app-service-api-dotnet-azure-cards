2fa51805c0c08e6cde450b0f2db6712215ad32f3
Brady Gaster <bradyg@microsoft.com>
2015-10-14 23:56:08 +0200

    improved the console demo

--- AzureCards.ConsoleClient/Program.cs
+++ AzureCards.ConsoleClient/Program.cs
@@ -11,48 +11,60 @@ namespace AzureCards.ConsoleClient
 {
     class Program
     {
-        private const string GW_URL = "YOUR GATEWAY URL";
+        private const string GW_URL = "http://devintersectionseu66c15fe52d0b446c920739d03a993afa.azurewebsites.net";
         private const string URL_TOKEN = "#token=";
 
         [STAThread]
         static void Main(string[] args)
         {
-            string deckId = string.Empty;
+            var appServiceClient = new AppServiceClient(GW_URL);
+            var deckClient = appServiceClient.CreateAzureCardsClient();
+            var deckId = deckClient.Deck.New();
 
+            Console.WriteLine(string.Format("Your new Deck ID is {0}", deckId));
+
+            // shuffle the deck
+            for(int i=0; i<10; i++)
+                deckClient.Deck.Shuffle(deckId);
+
+            // deal a hand and show it
+            var hand = deckClient.Deck.Deal(deckId, 11);
+            foreach (var card in hand.Cards)
+                Console.WriteLine($"{card.Face} of {card.Suit}");
+
+            Console.WriteLine("Press enter to exit");
+            Console.ReadLine();
+        }
+
+        #region
+        private static void AuthorizeClient(AppServiceClient appServiceClient)
+        {
             Form frm = new Form();
 
             WebBrowser browser = new WebBrowser();
             browser.Dock = DockStyle.Fill;
 
             browser.DocumentCompleted += (sender, e) =>
+            {
+                if (e.Url.AbsoluteUri.IndexOf(URL_TOKEN) > -1)
                 {
-                    if (e.Url.AbsoluteUri.IndexOf(URL_TOKEN) > -1)
-                    {
-                        var encodedJson = e.Url.AbsoluteUri.Substring(e.Url.AbsoluteUri.IndexOf(URL_TOKEN) + URL_TOKEN.Length);
-                        var decodedJson = Uri.UnescapeDataString(encodedJson);
-                        var result = JsonConvert.DeserializeObject<dynamic>(decodedJson);
-                        string userId = result.user.userId;
-                        string userToken = result.authenticationToken;
+                    var encodedJson = e.Url.AbsoluteUri.Substring(e.Url.AbsoluteUri.IndexOf(URL_TOKEN) + URL_TOKEN.Length);
+                    var decodedJson = Uri.UnescapeDataString(encodedJson);
+                    var result = JsonConvert.DeserializeObject<dynamic>(decodedJson);
+                    string userId = result.user.userId;
+                    string userToken = result.authenticationToken;
 
-                        var appServiceClient = new AppServiceClient(GW_URL);
-                        appServiceClient.SetCurrentUser(userId, userToken);
+                    appServiceClient.SetCurrentUser(userId, userToken);
 
-                        var deckClient = appServiceClient.CreateAzureCardsClient();
-                        deckId = deckClient.Deck.New();
-
-                        frm.Close();
-                    }
-                };
+                    frm.Close();
+                }
+            };
 
             browser.Navigate(string.Format(@"{0}login/twitter", GW_URL));
 
             frm.Controls.Add(browser);
             frm.ShowDialog();
-
-            Console.WriteLine(string.Format("Your new Deck ID is {0}", deckId));
-
-            Console.WriteLine("Press enter to exit");
-            Console.ReadLine();
         }
+        #endregion
     }
 }
